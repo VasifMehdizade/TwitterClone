2ad57f29cf35a6a6eb9fc3ac53f96e8b747a03db
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2023-08-17 21:28:41 +0400

    Initial commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Vasif Mehdi on 17.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
