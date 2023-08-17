ef6ae5ffdd5f3a96bbc52bdb13907a7df8a2b77a
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2023-08-17 21:35:10 +0400

    Storyboard deleted
    LoginPage folder created

--- TwitterClone/LoginPage/View/LoginController.swift
+++ TwitterClone/LoginPage/View/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  TwitterClone
 //
 //  Created by Vasif Mehdi on 17.08.23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
-
 }
-
